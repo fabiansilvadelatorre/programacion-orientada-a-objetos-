10cb41c7db5b48f2f4762c49645b88abb2adf170
fabiansilvadelatorre <fabiansilvadelatorre@gmail.com>
2023-02-12 19:14:30 -0600

    :)

---
+++ src/reto3/proces/Proces.java
@@ -0,0 +1,15 @@
+package reto3.proces;
+/**
+ * Emetodos de convercio numericos
+ */
+public class Proces {
+    /**
+     * Este metodo recibe un nstring que representa un numero en base decimal
+     *      * y regresa el mismo numero en su representacion de base binaria
+     *      *
+     *      * @param decimal numero a convertir en base decimal
+     *      * @return numero en base binaria
+     */
+
+    }
+
